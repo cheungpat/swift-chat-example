f4abe20df3a3ec7554cdb5b9fd804d5eded23369
Joey <a821246711@gmail.com>
2016-08-29 12:02:06 +0800

    add create direct conversation

--- chat-demo/CreateDirectConversation.swift
+++ chat-demo/CreateDirectConversation.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2016 Oursky Ltd. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SKYKit
+
+class CreateDirectConversation: UIViewController {
+    
+    @IBOutlet var userIdTextField: UITextField!
+    @IBOutlet var createdConversationTextView: UITextView!
+    @IBOutlet var createBtn: UIButton!
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func createConversation(sneder: AnyObject!) {
+        if var id = userIdTextField.text where !id.isEmpty {
+            
+            if id.hasPrefix("user/") {
+                id = id.substringFromIndex("user/".endIndex)
+            }
+            
+            SKYContainer.defaultContainer().getOrCreateDirectConversationWithuUserId(id, completionHandler: { (conversation, error) in
+                if error != nil {
+                    let alert = UIAlertController(title: "Unable to create direct conversation", message: error.localizedDescription, preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    return
+                }
+                
+                self.createdConversationTextView.text = conversation.recordID.canonicalString
+            })
+        }
+    }
+}
